104757ad85df1af7c9bb12c0daf5913e35fc88a5
Dennise Catolos <63337204+dentolos19@users.noreply.github.com>
2021-04-09 15:28:52 +0800

    Updated unit testing

--- PasteMystNet.Tests/Operations.cs
+++ PasteMystNet.Tests/Operations.cs
@@ -9,6 +9,7 @@ namespace PasteMystNet.Tests
     internal static class Operations
     {
 
+        private static PasteMystAuth UserAuth { get; set; }
         private static PasteMystPasteForm TemplateForm { get; set; }
 
         [SetUp]
@@ -44,9 +45,29 @@ namespace PasteMystNet.Tests
         }
 
         [Test]
+        public static async Task PatchPasteTest()
+        {
+            if (UserAuth is null)
+                return;
+            var paste = await TemplateForm.PostPasteAsync(UserAuth);
+            var edit = paste.CreateEditForm();
+            edit.Title += " (Edited)";
+            _ = await edit.PatchPasteAsync(UserAuth);
+        }
+        
+        [Test]
         public static async Task GetPasteTest()
         {
             _ = await PasteMystPaste.GetPasteAsync("4jec5of5");
+        }
+
+        [Test]
+        public static async Task DeletePasteTest()
+        {
+            if (UserAuth is null)
+                return;
+            var paste = await TemplateForm.PostPasteAsync(UserAuth);
+            await PasteMystPaste.DeletePasteAsync(paste.Id, UserAuth);
         }
 
         [Test]
